823f08aba8d390b952d2ed6199a3280549568513
Amol Khaware <Amol.Khaware@elliemae.com>
2017-12-29 09:09:49 +0530

    Base Maven Project

---
+++ src/test/java/DummyMavenProject/DummyMavenProject/TestClass.java
@@ -0,0 +1,12 @@
+package DummyMavenProject.DummyMavenProject;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("First Maven");
+	}
+
+}
